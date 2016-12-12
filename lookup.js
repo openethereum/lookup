e35847d6fbd70f524542e38856e1093f945c5b0e
Jannis R <mail@jannisr.de>
2016-12-12 16:36:13 +0100

    respond with address

--- lookup.js
+++ lookup.js
@@ -4,8 +4,21 @@ const co = require('co-express')
 const boom = require('boom')
 const sha3 = require('web3/lib/utils/sha3')
 
-const web3 = require('./lib/web3')
+const addressByEmailHash = require('./lib/address-by-email-hash')
 
 module.exports = co(function* (req, res) {
-  // todo
+  const email = req.query.email
+  if (typeof email !== 'string' || email.indexOf('@') < 0) throw boom.badRequest('E-mail is invalid.')
+
+  const emailHash = '0x' + sha3(email)
+  const data = {}
+
+  try {
+    data.address = yield addressByEmailHash(emailHash)
+  } catch (err) {
+    console.error(err)
+    throw boom.internal('An error occured while querying Parity.')
+  }
+
+  res.json(data)
 })
