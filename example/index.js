d1fa51445d56320db9d1a16766a90bc38fcfe628
Nicolas Gotchac <ngotchac@gmail.com>
2017-02-01 14:07:17 +0100

    Fix bugs + examples

---
+++ example/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const Web3 = require('web3')
+
+const Lookup = require('../')
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+Lookup
+  .addressByName(web3, 'ngotchac')
+  .then((result) => {
+    console.log('looked up "ngotchac"', result)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
