fdf35bbd6b325ab91e2393e25ee41f7f0aacb320
Nicolas Gotchac <ngotchac@gmail.com>
2017-02-01 17:51:55 +0100

    Optional `api` param

--- example/index.js
+++ example/index.js
@@ -5,15 +5,17 @@ const {Api} = require('@parity/parity.js')
 // do the setup
 const transport = new Api.Transport.Http('http://localhost:8545')
 const api = new Api(transport)
+api.transport._connectTimeout = -1;
 
 const {sha3} = require('../src/util')
 
 const Lookup = require('../')
 
+// Lookup.init(api, { registryAddress: '0xdeadbeafdeadbeafdeadbeafdeadbeafdeadbeaf' })
 Lookup.init(api)
   .then(() => {
     Lookup
-      .byName(api, 'ngotchac')
+      .byName('ngotchac')
       .then((result) => {
         console.log('=> looked up name "ngotchac":')
         console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
@@ -24,9 +26,9 @@ Lookup.init(api)
       })
 
     Lookup
-      .byAddress(api, '0x639ba260535db072a41115c472830846e4e9ad0f')
+      .byAddress('0x639ba260535db072a41115c472830846e4e9ad0f')
       .then((result) => {
-        console.log('looked up address "0x639ba260535db072a41115c472830846e4e9ad0f":')
+        console.log('=> looked up address "0x639ba260535db072a41115c472830846e4e9ad0f":')
         console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
         console.log('')
       })
@@ -35,9 +37,9 @@ Lookup.init(api)
       })
 
     Lookup
-      .byEmail(api, sha3('ngotchac@gmail.com'))
+      .byEmail(sha3('ngotchac@gmail.com'))
       .then((result) => {
-        console.log('looked up address "ngotchac@gmail.com":')
+        console.log('=> looked up address "ngotchac@gmail.com":')
         console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
         console.log('')
       })
