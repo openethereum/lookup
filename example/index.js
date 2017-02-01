86fd784a65bacd1fda9e186dd06c86012456a090
Jannis R <mail@jannisr.de>
2017-02-01 18:45:51 +0100

    docs :memo:, nice printing in examples

--- example/index.js
+++ example/index.js
@@ -1,6 +1,10 @@
 'use strict'
 
+const util = require('util')
 const {Api} = require('@parity/parity.js')
+
+const print = (val) => util.inspect(val, {depth: null, colors: true})
+const indent = (str) => '    ' + str.split('\n').join('\n    ') + '\n'
 
 // do the setup
 const transport = new Api.Transport.Http('http://localhost:8545')
@@ -16,8 +20,7 @@ lookup
   .byName('ngotchac')
   .then((result) => {
     console.log('=> looked up name "ngotchac":')
-    console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
-    console.log('')
+    console.log(indent(print(result)))
   })
   .catch((error) => {
     console.error(error)
@@ -27,8 +30,7 @@ lookup
   .byAddress('0x639ba260535db072a41115c472830846e4e9ad0f')
   .then((result) => {
     console.log('=> looked up address "0x639ba260535db072a41115c472830846e4e9ad0f":')
-    console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
-    console.log('')
+    console.log(indent(print(result)))
   })
   .catch((error) => {
     console.error(error)
@@ -38,8 +40,7 @@ lookup
   .byEmail(sha3('ngotchac@gmail.com'))
   .then((result) => {
     console.log('=> looked up address "ngotchac@gmail.com":')
-    console.log('     ' + JSON.stringify(result, null, 2).split('\n').join('\n      '))
-    console.log('')
+    console.log(indent(print(result)))
   })
   .catch((error) => {
     console.error(error)
--- readme.md
+++ readme.md
@@ -32,4 +32,37 @@ lookup
   .then(console.log)
 ```
 
+You will get a result like this:
+
+```js
+{
+  address: '0x639ba260535db072a41115c472830846e4e9ad0f',
+  name: 'ngotchac',
+  badges: [ {
+    id: 12,
+    address: '0x4A99350b039068fD326a318C599Be2E09E657D4A',
+    name: 'emailverification',
+    title: 'eMail verified',
+    img: 'https://raw.githubusercontent.com/ethcore/dapp-assets/master/certifications/email-verification.svg'
+  } ],
+  tokens: [
+    {
+      id: 0,
+      address: '0xAde20230C260f1fd46cD70174dd58363929fEa31',
+      TLA: 'GOT',
+      base: { [String: '1000000'] s: 1, e: 6, c: [ 1000000 ] }, // BigNumber
+      name: 'Gotchacoin',
+      img: 'http://vignette3.wikia.nocookie.net/farmville2/images/2/26/Baby_Caramel_Pygora_Goat.png/revision/latest?cb=20130201193031',
+      balance: '3'
+    }, {
+      TLA: 'ETH',
+      name: 'Ether',
+      base: { [String: '1000000000000000000'] s: 1, e: 18, c: [ 10000 ] },
+      img: 'https://raw.githubusercontent.com/ethcore/parity/1e6a2cb/js/assets/images/contracts/ethereum-black-64x64.png',
+      balance: '12.5245565653765155'
+    }
+  ]
+}
+```
+
 Refer to [the example](example/index.js) for more use cases.
